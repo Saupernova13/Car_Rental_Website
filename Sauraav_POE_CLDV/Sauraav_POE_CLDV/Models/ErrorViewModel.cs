21a4e6b9466c6e35ecffd54ac068ff41921b2ea5
Sauraav Jayrajh <69242316+Saupernova13@users.noreply.github.com>
2023-06-19 16:23:29 +0200

    Initial Commit Of Project Files
    Project Files are Created

---
+++ Sauraav_POE_CLDV/Sauraav_POE_CLDV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sauraav_POE_CLDV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
