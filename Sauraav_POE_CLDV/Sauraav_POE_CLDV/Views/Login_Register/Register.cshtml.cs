bced49fcd3c987ca013a19a0ca45f3bbf10e885e
Sauraav Jayrajh <69242316+Saupernova13@users.noreply.github.com>
2023-06-25 01:34:30 +0200

    Homepage completed
    The home page has been fully created and completed.

---
+++ Sauraav_POE_CLDV/Sauraav_POE_CLDV/Views/Login_Register/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sauraav_POE_CLDV.Views.Login_Register
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
