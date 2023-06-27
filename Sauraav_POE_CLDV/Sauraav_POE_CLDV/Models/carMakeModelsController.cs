c2145b34b619fbcd1787d68566695863a6770094
Sauraav Jayrajh <69242316+Saupernova13@users.noreply.github.com>
2023-06-27 02:30:25 +0200

    Working Login/Registration System
    Working Login/Registration System

--- Sauraav_POE_CLDV/Sauraav_POE_CLDV/Models/carMakeModelsController.cs
+++ Sauraav_POE_CLDV/Sauraav_POE_CLDV/Models/carMakeModelsController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using Sauraav_POE_CLDV.Data;
 
 namespace Sauraav_POE_CLDV.Models
 {
+    [Authorize]
     public class carMakeModelsController : Controller
     {
         private readonly ConnectDB _context;
